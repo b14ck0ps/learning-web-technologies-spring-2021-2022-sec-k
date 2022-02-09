8aee74526a184615ad892463915c024e85206e53
Azran Hossain <mr69751@gmail.com>
2022-02-09 11:37:41 +0600

    full complete

--- MID_LAB_TASK_04_PHP/Task_8.php
+++ MID_LAB_TASK_04_PHP/Task_8.php
@@ -4,6 +4,9 @@
         [1,2,'B','C'],
         [1,'D','E','F']
     ];
+    echo "<table border=1>";
+    echo "<tr>";
+    echo "<td>";
     foreach ($array as $i => $arr){
         foreach ($arr as $i){
             if(is_int($i)){
@@ -12,5 +15,16 @@
         }
         echo "<br>";
      }
-
+     echo "</td>";
+     echo "<td>";
+     foreach ($array as $i => $arr){
+        foreach ($arr as $i){
+            if(!is_int($i)){
+                echo $i." ";
+            }
+        }
+        echo "<br>";
+     }
+     echo "</td>";
+     echo "</tr>";
 ?>
