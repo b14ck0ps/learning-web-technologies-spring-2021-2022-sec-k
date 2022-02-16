9db5c16b14f764a3b0ffb483474267ae0d441eb2
Azran Hossain <mr69751@gmail.com>
2022-02-16 11:53:11 +0600

    class commit

---
+++ ASSESSMENT TASK/public_home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Public Home</title>
+</head>
+
+<body>
+    <table border="2" style="border-collapse: collapse;" width="100%">
+        <tr>
+            <td style="border:none;">
+                <img src="https://i.ibb.co/Sm8cSr0/image.png" alt="logo"></a>
+            </td>
+            <td style="border:none; text-align: right">
+                <a href="#Home">Home</a> |
+                <a href="#Login">Login</a> |
+                <a href="#Registration">Registration</a>
+            </td>
+        </tr>
+    </table>
+    <table border="2" style="border-collapse: collapse;" width="100%">
+    <tr>
+        <td style="border:none;">
+            <h2>Welcome Home</h2>
+        </td>
+        
+    </tr>
+    </table>
+</body>
+
+</html>
