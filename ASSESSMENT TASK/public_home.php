e1c20c3ff241960b54aadf6b652eb9184cef11fb
Azran Hossain <mr69751@gmail.com>
2022-02-16 12:14:38 +0600

    🔥

--- ASSESSMENT TASK/public_home.php
+++ ASSESSMENT TASK/public_home.php
@@ -12,22 +12,33 @@
     <table border="2" style="border-collapse: collapse;" width="100%">
         <tr>
             <td style="border:none;">
-                <img src="https://i.ibb.co/Sm8cSr0/image.png" alt="logo"></a>
+                <img  src="https://i.ibb.co/Sm8cSr0/image.png" alt="logo"></a>
             </td>
-            <td style="border:none; text-align: right">
+            <td style=" padding:12px; border:none; text-align: right">
                 <a href="#Home">Home</a> |
                 <a href="#Login">Login</a> |
                 <a href="#Registration">Registration</a>
             </td>
         </tr>
-    </table>
-    <table border="2" style="border-collapse: collapse;" width="100%">
-    <tr>
-        <td style="border:none;">
-            <h2>Welcome Home</h2>
+        <tr>
+            <td colspan="2" style="border:none;">
+                <hr style="border: none; border-bottom: 2px solid gray;">
+            </td>
+        </tr>
+        <td style="border:none; padding:10px;height: 300px; vertical-align: top; text-align: left;">
+            <h1>Welcome to xCompany</h1>
         </td>
-        
-    </tr>
+        </tr>
+        <tr>
+            <td colspan="2" style="border:none;">
+                <hr style="border: none; border-bottom: 2px solid gray;">
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border:none; text-align: center">
+                Copyright &copy; 2017
+            </td>
+        </tr>
     </table>
 </body>
 
