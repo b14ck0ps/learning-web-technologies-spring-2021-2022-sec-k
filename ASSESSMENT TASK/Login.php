124360ff8bac16eb8bd27a6f68f39f3dafa4cee8
Azran Hossain <mr69751@gmail.com>
2022-02-16 12:55:46 +0600

    structure 👍

---
+++ ASSESSMENT TASK/Login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Public Home</title>
+</head>
+
+<body>
+    <table border="2" style="border-collapse: collapse;" width="100%">
+        <tr>
+            <td style="border:none;">
+                <img src="https://i.ibb.co/Sm8cSr0/image.png" alt="logo"></a>
+            </td>
+            <td style=" padding:12px; border:none; text-align: right">
+                <a href="#Home">Home</a> |
+                <a href="#Login">Login</a> |
+                <a href="#Registration">Registration</a>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border:none;">
+                <hr style="border: none; border-bottom: 2px solid gray;">
+            </td>
+        </tr>
+        <td colspan="2" align="center" valign="center" style="border:none; padding:10px;height: 300px;">
+            <fieldset style="display: inline-block">
+                <legend>LOGIN</legend>
+                <table>
+                    <tr>
+                        <td>
+                            User Name
+                        </td>
+                        <td>
+                            : <input type="text" name="username"><br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Password
+                        </td>
+                        <td>
+                            : <input type="password" name="password"><br>
+                        </td>
+                    </tr>
+                </table>
+                <hr>
+                <div align=left>
+                    <input type="checkbox" name="remember"> Remember me <br> <br>
+                    <input type="submit" name="login" value="Submit"> <a href="">Forgot Password?</a>
+                </div>
+
+            </fieldset>
+        </td>
+        </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border:none;">
+                <hr style="border: none; border-bottom: 2px solid gray;">
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border:none; text-align: center">
+                Copyright &copy; 2017
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
