493e6068230e309dbaa673d3ae398359f4510306
Azran Hossain <mr69751@gmail.com>
2022-02-16 13:26:27 +0600

    struct 👍

---
+++ ASSESSMENT TASK/Dashboard.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Public Home</title>
+</head>
+
+<body>
+    <table border="2" style="border-collapse: collapse;" width="100%">
+        <tr style="border: 2px solid black;">
+            <td style="border-right: 0px solid">
+                <img  src="https://i.ibb.co/Sm8cSr0/image.png" alt="logo"></a>
+            </td>
+            <td style="padding:12px; border-bottom: 5px single black collapse; border-left: 0px solid; text-align: right">
+                Logged in as
+                <a href="#profile">Bob</a> |
+                <a href="#logout">Logout</a>
+            </td>
+        </tr>
+        <tr style="border: 2px solid black;">
+            <td style="border: 2px solid black; width: 30%;">
+                <b>Account</b> <br>
+                <hr>
+                <ul>
+                    <li><a href="Dashboard.php">Dashboard</a></li>
+                    <li><a href="profile.php">View Profile</a></li>
+                    <li><a href="edit_profile.php">Edit Profile</a></li>
+                    <li><a href="change_dp.php">Change Profile Picture</a></li>
+                    <li><a href="change_password.php">Chnage Password</a></li>
+                    <li><a href="logout.php">Log out</a></li>
+                </ul>
+            </td>
+            <td style="vertical-align: top; text-align: left;padding: 5px;">
+                <h4>Welcome Bob</h4>
+            </td>
+        </tr>
+        </tr>
+        <tr style="border: 2px solid black;">
+            <td colspan="2" style="border:none; text-align: center">
+                Copyright &copy; 2017
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
