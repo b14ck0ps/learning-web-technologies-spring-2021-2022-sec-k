07caa47aaccad7c680786d780f15d91e41782439
Azran Hossain <mr69751@gmail.com>
2022-02-16 13:28:48 +0600

    struct 👍

---
+++ ASSESSMENT TASK/Forgot_password.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Public Home</title>
+</head>
+
+<body>
+    <table border="2" style="border-collapse: collapse;" width="100%">
+        <tr style="border: 2px solid black;">
+            <td style="border:none;">
+                <img src="https://i.ibb.co/Sm8cSr0/image.png" alt="logo"></a>
+            </td>
+            <td style=" padding:12px; border:none; text-align: right">
+                <a href="#Home">Home</a> |
+                <a href="#Login">Login</a> |
+                <a href="#Registration">Registration</a>
+            </td>
+        </tr>
+        <td colspan="2" align="center" valign="center" style="border:none; padding:10px;height: 300px;">
+            <fieldset style="display: inline-block">
+                <legend>FORGOT PASSWORD</legend>
+                <table>
+                    <tr>
+                        <td>
+                            Enter Email
+                        </td>
+                        <td>
+                            : <input type="email" name="email"><br>
+                        </td>
+                    </tr>
+                    <tr>
+                    </tr>
+                </table>
+                <hr>
+                <div align=left>
+                    <input type="submit" name="login" value="Submit">
+                </div>
+
+            </fieldset>
+        </td>
+        </td>
+        </tr>
+        <tr style="border: 2px solid black;">
+            <td colspan="2" style="border:none; text-align: center; padding: 15px;">
+                Copyright &copy; 2017
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
