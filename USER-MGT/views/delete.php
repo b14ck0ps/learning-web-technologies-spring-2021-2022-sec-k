3daeb08cd3f0dc459dd369b3d411394ed38c7765
Azran Hossain <mr69751@gmail.com>
2022-03-02 12:25:16 +0600

    ask for confirmation

--- USER-MGT/views/delete.php
+++ USER-MGT/views/delete.php
@@ -1,3 +1,18 @@
 <?php 
-	include('header.php');
+	require_once('../controllers/header.php');
+	$user = $_REQUEST['id'];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Delete user</title>
+</head>
+<body>
+	<center>
+	<a href='../controllers/delete.php?id=<?=$user?>&delete=del'> Confirm Delete </a> | <a href="../views/userlist.php">Cancle</a>
+	</center>
+</body>
+</html>
